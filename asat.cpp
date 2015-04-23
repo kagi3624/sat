4b247d9ec2db2c9926a1f4a2bf867862d2c1206e
Roman <roman.bleim@uni-oldenburg.de>
2015-04-23 17:41:07 +0200

    major optimization to asat

--- asat.cpp
+++ asat.cpp
@@ -3,7 +3,27 @@
 using namespace boost::random;
 
 //returns the indices of unsatisfied clauses
+
 void find_unsatisfied_clauses(const sat_prob &A, const std::vector<int> &conf, std::vector<int> &R){
+
+
+	for(unsigned int i = 0; i<A.get_num_clauses();++i){
+		int l_value = 0;
+		int r_value = 1;
+		for(unsigned int j = 0; j<A.get_clause(i).v.size();++j){
+			if(A.get_clause(i).get_literal(j)<0){
+				l_value -= conf[-A.get_clause(i).get_literal(j)-1];
+				r_value--;
+			}
+			else
+				l_value += conf[A.get_clause(i).get_literal(j)-1];
+		}
+		if(l_value<r_value)
+			R.push_back(i);
+	}
+}
+
+void find_unsatisfied_clauses(const sat_prob &A, const std::vector<int> &conf, std::vector<int> &R, std::vector<int> &W){
 
 	R.clear();
 
@@ -46,7 +66,7 @@ std::vector<int> solve_asat (sat_prob &A, double p){
 	std::vector<int> R,U;
 	std::vector<int> configuration;
 	
-	std::vector<std::vector<int> > Cfx = find_clauses_for_var(A);
+	std::vector<std::vector<int> > CFx = find_clauses_for_var(A);
 
 	for(unsigned int i = 0; i<A.get_num_variables();++i)
 		configuration.push_back(uniform_int_distribution<>(0, 1)(g));
@@ -67,24 +87,24 @@ std::vector<int> solve_asat (sat_prob &A, double p){
 		std::size_t z = uniform_int_distribution<>(0,A.get_clause(w).v.size()-1)(g);
 		
 		if(configuration[abs(A.get_clause(w).v[z])-1] == 0)	
-			changed_configuration[abs(A.get_clause(w).v[z])-1] = 1;
+			configuration[abs(A.get_clause(w).v[z])-1] = 1;
 		else
-			changed_configuration[abs(A.get_clause(w).v[z])-1] = 0;
+			configuration[abs(A.get_clause(w).v[z])-1] = 0;
 			
-		find_unsatisfied_clauses(A,changed_configuration,R);
-		if(R.size()<=U.size()){
-			configuration[abs(A.get_clause(w).v[z])-1]=changed_configuration[abs(A.get_clause(w).v[z])-1];
-			//std::cout<<"a"<<"    "<<R.size()<<" "<<U.size()<<"  "<<t<<'\n'<<std::flush;
-			U = R;
+			
+		find_unsatisfied_clauses(A,configuration,R,CFx[abs(A.get_clause(w).v[z])-1]);
+		if(R.size()>U.size() && uniform_real_distribution<>(0.0,1.0)(g)>p){
+			if(configuration[abs(A.get_clause(w).v[z])-1] == 0)	
+				configuration[abs(A.get_clause(w).v[z])-1] = 1;
+			else
+				configuration[abs(A.get_clause(w).v[z])-1] = 0;
+			std::cout<<"a    ------"<<"  "<<t<<'\n'<<std::flush;
 		}
-		else if(uniform_real_distribution<>(0.0,1.0)(g)<=p){
-			configuration[abs(A.get_clause(w).v[z])-1]=changed_configuration[abs(A.get_clause(w).v[z])-1];
-			//std::cout<<"b"<<"    "<<R.size()<<" "<<U.size()<<"  "<<t<<'\n'<<std::flush;
+
+		else{
 			U = R;
+			std::cout<<"b    ------"<<"  "<<t<<'\n'<<std::flush;
 		}
-		/*else{
-			std::cout<<"c    ------"<<"  "<<t<<'\n'<<std::flush;
-		}*/
 	}
 	std::cout<<"t_max reached\n";
 	return configuration;
